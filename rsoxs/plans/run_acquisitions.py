42face717720cd6cb836974b25e64974fde0f5a5
PriyankaKetkarBNL <pketkar@bnl.gov>
2025-08-15 14:51:41 -0400

    Removed explicit sample_id argument in nbs_energy_scan after merging changes from main branch.

--- rsoxs/plans/run_acquisitions.py
+++ rsoxs/plans/run_acquisitions.py
@@ -164,7 +164,6 @@ def run_acquisitions_single(
                                 dwell=acquisition["exposure_time"],
                                 n_exposures=acquisition["exposures_per_energy"], 
                                 group_name=acquisition["group_name"],
-                                sample=acquisition["sample_id"],
                                 )
                     
                     ## If cycles is an integer > 0, then run pairs of sweeps going in ascending then descending order of energy
@@ -176,7 +175,6 @@ def run_acquisitions_single(
                                 dwell=acquisition["exposure_time"],
                                 n_exposures=acquisition["exposures_per_energy"], 
                                 group_name=acquisition["group_name"],
-                                sample=acquisition["sample_id"],
                                 )
                             yield from nbs_energy_scan(
                                 *energy_parameters[::-1], ## Reverse the energy list parameters to produce reversed energy list
@@ -184,7 +182,6 @@ def run_acquisitions_single(
                                 dwell=acquisition["exposure_time"],
                                 n_exposures=acquisition["exposures_per_energy"], 
                                 group_name=acquisition["group_name"],
-                                sample=acquisition["sample_id"],
                                 )
                     
                     ## TODO: maybe default to cycles = 1?  It would be good practice to have forward and reverse scan to assess reproducibility
