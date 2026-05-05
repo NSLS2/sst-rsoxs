64c6081ba98d8b8803d5e269cf9bd60154e874dc
Charles Titus <ctitus@bnl.gov>
2026-05-05 12:59:17 -0400

    Adding sim_startup to set initial conditions for sim

---
+++ rsoxs/sim_startup.py
@@ -0,0 +1,40 @@
+from nbs_bl.beamline import GLOBAL_BEAMLINE as bl
+from datetime import datetime
+import os
+from os.path import join
+from rsoxs.HW.contingencies import turn_off_checks
+
+md = bl.md
+md['data_session'] = '111111'
+md['cycle'] = '2026-2'
+md['username'] = 'simuser'
+md['start_datetime'] = datetime.now().isoformat()
+md['saf'] = '222222'
+md['proposal'] = {
+    'proposal_id': '111111',
+    'title': 'Test Proposal',
+    'type': 'GU',
+    'pi_name': 'PI, Test'
+}
+
+def get_proposal_directory(asset_name):
+    write_path_template = "/nsls2/data/sst/proposals"
+    date_template = "%Y/%m/%d/"
+    proposal_path = f"{md['cycle']}/{md['data_session']}/assets/{asset_name}"
+    write_path = join(write_path_template, proposal_path, date_template)
+    formatter = datetime.now().strftime
+    write_path = formatter(write_path)
+    return write_path
+
+def create_proposal_directory(asset_name):
+    write_path = get_proposal_directory(asset_name)
+    if not os.path.exists(write_path):
+        os.makedirs(write_path)
+    return write_path
+
+for detector in bl.all_standard_pros.devices.values():
+    if hasattr(detector, "tiff"):
+        camera_name = detector.tiff.camera_name
+        create_proposal_directory(camera_name)
+
+turn_off_checks()
